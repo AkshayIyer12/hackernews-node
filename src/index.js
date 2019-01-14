68611b5b69116bcae40bf2a405102aae756c35ed
Akshayiyer12 <c.m.akshay.iyer@gmail.com>
2019-01-14 14:52:41 +0530

    Add resolvers for mutation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,42 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-type Query {
-  info: String!
-}
-`
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+},
+	    {
+    id: 'link-120',
+    url: 'www.facebook.com',
+    description: 'Fuck facebook.com!'
+}]
+
+let idCount = links.length
 const resolvers = {
     Query: {
-	info: () => `This is the API of a Hackernews clone` 
+	info: () => `This is the API of a Hackernews clone`,
+	feed: () => links,
+    },
+    Mutation: {
+	post: (parent, args) => {
+	    const link = {
+		id: `link-${idCount++}`,
+		description: args.description,
+		url: args.url
+	    }
+	    links.push(link)
+	    return link
+	}
+    },
+    Link: {
+	id: parent => parent.id,
+	description: parent => parent.description,
+	url: parent => parent.url,
     }
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers
 })
 
